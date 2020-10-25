0fcca3648d2ac9b2baafb29d09f1af23854dfbfe
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-10-25 03:17:58 -0300

    testando exemplo de useCallback e useMemo

--- tic-tac-toe/src/pages/Home.js
+++ tic-tac-toe/src/pages/Home.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 
-// import { Container } from './styles';
+import GameContext from '../contexts/GameContext';
+import Board from '../components/Board';
+import './Home.css';
+import { useFetch } from '../utils/useFetch';
+import { useCountRender } from '../utils/useCountRender';
+
 
 function Home() {
-  return <div>TIC-TAC-TOE</div>;
+  const [count, setCount] = useState(0);
+
+  const { data } = useFetch(
+    'https://raw.githubusercontent.com/ajzbc/kanye.rest/quotes/quotes.json'
+  );
+
+  // useCountRender();
+  const getLongestWord= useCallback((array) => {
+    if (!array) {
+      return [];
+    }
+    let longestWord = '';
+    console.log("teste");
+
+    JSON.parse(array).forEach((sentence) => {
+      sentence.split(' ').forEach((word) => {
+        if (word.length > longestWord.length) {
+          longestWord = word;
+        }
+      });
+    });
+
+    
+    return longestWord;
+  }, [])
+
+  const longestWord = useMemo(() => getLongestWord(data), [data, getLongestWord])
+
+  const handleClick = useCallback((n) => {
+    setCount((c) => c + n);
+  }, [setCount]);
+
+  return (
+    <>
+      {/* <GameContext> */}
+      <button onClick={handleClick}>{count}</button>
+      <Board handleClick={handleClick} count={count} />
+      {/* </GameContext>   */}
+      <div>{longestWord}</div>
+    </>
+  );
 }
 
 export default Home;
