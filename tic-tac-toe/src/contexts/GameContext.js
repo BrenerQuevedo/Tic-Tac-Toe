e79180b1dd422df67d7513d96cb3450c5961ec40
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-11-23 03:52:05 -0300

    terminando contexto do jogo por agora

--- tic-tac-toe/src/contexts/GameContext.js
+++ tic-tac-toe/src/contexts/GameContext.js
@@ -1,16 +1,13 @@
 import React, { createContext, useState, useReducer } from 'react';
 
-import types from 'prop-types';
+import propTypes from 'prop-types';
 
-import GameReducer, {INITIAL_STATE} from "./GameReducer"
-
+import GameReducer, { INITIAL_STATE } from './GameReducer';
 
 export const GameContext = createContext();
 
 export default function GameContextProvider({ children }) {
-
-  const [state, dispatch] = useReducer(GameReducer, INITIAL_STATE)
-
+  const [state, dispatch] = useReducer(GameReducer, INITIAL_STATE);
 
   return (
     <GameContext.Provider value={{ state, dispatch }}>
@@ -20,5 +17,5 @@ export default function GameContextProvider({ children }) {
 }
 
 GameContextProvider.propTypes = {
-  children: types.node.isRequired
-}
+  children: propTypes.node.isRequired,
+};
